4ef1e01ba4eb7586758c2868078e69e8b29a16ac
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-07-13 16:34:28 +0200

    changed border colors in script from rgb to hex

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const submitBtn = document.getElementById("submit_btn");
 validatePassword = function () {
   if (password.value != confirmPassword.value) {
     passwordValidationText.textContent = "*Passwords don`t match";
-    password.style.borderColor = rgb(255, 0, 0);
-    confirmPassword.style.borderColor = rgb(255, 0, 0);
+    password.style.borderColor = "#FF0000";
+    confirmPassword.style.borderColor = "#FF0000";
   }
   if (password.value === confirmPassword.value) {
     passwordValidationText.textContent = "";
-    password.style.borderColor = rgb(0, 128, 0);
-    confirmPassword.style.borderColor = rgb(0, 128, 0);
+    password.style.borderColor = "#008000";
+    confirmPassword.style.borderColor = "#008000";
   }
 };
 
